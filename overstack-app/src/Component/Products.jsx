b6a481de7f94e665596ce714e57d5ff617659ab9
Ashutosh Mahapatra <94985469+Ashutosh-041298-DEV@users.noreply.github.com>
2025-03-29 18:59:17 +0530

    slow api issue

--- overstack-app/src/Component/Products.jsx
+++ overstack-app/src/Component/Products.jsx
@@ -1,81 +1,70 @@
 import React, { useEffect, useState } from "react";
 import "./Mugs.css";
 import {
-  ChevronDownIcon,
-  CheckCircleIcon,
   ChevronRightIcon,
   ChevronLeftIcon,
 } from "@chakra-ui/icons";
-
 import { Button, Flex, Spinner, useToast } from "@chakra-ui/react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
-import { Icon } from "@chakra-ui/react";
-import { FiHeart } from "react-icons/fi";
 import { useSelector } from "react-redux";
 
 const Products = () => {
   const toast = useToast();
   const { category: initialCategory } = useParams();
+  const { isAdmin } = useSelector((state) => state);
+
   const [state, setState] = useState({
     category: initialCategory,
     page: 1,
     order: "asc",
     data: [],
-    loading: false,
-    color: false,
+    loading: true,
+    totalPages: 1, // Track total pages
   });
 
-  const { isAdmin } = useSelector((state) => state);
- 
-// CALLING THE API WHENEVER THERE IS A CHANGE IN THE COMPONENTS
+  // Fetch data when category, page, or order changes
   useEffect(() => {
-    fetchData();
+    fetchData(state.category, state.page, state.order);
   }, [state.category, state.page, state.order]);
 
-// CHANGING DATA AS PER THE CHANGE OF PAGE
+  // Reset page when category changes
   useEffect(() => {
     setState((prevState) => ({
       ...prevState,
-      page: 1,
       category: initialCategory,
-      order: "asc"
+      page: 1,
     }));
   }, [initialCategory]);
 
-  const fetchData = () => {
-    setState((prevState) => ({
-      ...prevState,
-      loading: true,
-    }));
+  const fetchData = (category, page, order) => {
+    setState((prevState) => ({ ...prevState, loading: true }));
 
     axios
       .get(
-        `https://stock-server.onrender.com/products?category=${state.category}&_limit=12&_page=${state.page}&_sort=price&_order=${state.order}`
+        `https://stock-server.onrender.com/products?category=${category}&_limit=12&_page=${page}&_sort=price&_order=${order}`
       )
       .then((r) => {
         setState((prevState) => ({
           ...prevState,
           loading: false,
           data: r.data,
+          totalPages: r.data.length < 12 ? prevState.page : prevState.page + 1, // Update total pages dynamically
         }));
-        console.log(r.data);
       })
-      .catch((err) => {
-        console.log(err);
-      });
+      .catch((err) => console.log(err));
   };
 
   const handleDelete = (id) => {
     axios
       .delete(`https://stock-server.onrender.com/products/${id}`)
-      .then((res) => {
+      .then(() => {
         setState((prevState) => ({
           ...prevState,
           data: prevState.data.filter((e) => e.id !== id),
         }));
         toast({
-          title: "Delete Successfull.",
+          title: "Delete Successful.",
           status: "success",
           duration: 3000,
           isClosable: true,
@@ -84,52 +73,12 @@ const Products = () => {
       .catch((err) => console.log(err));
   };
 
-  const toTitleCase = (str) => {
-    return str.replace(/\w\S*/g, function (txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-  };
-
-  const handleHeart = (id) => {
-    setState((prevState) => ({
-      ...prevState,
-      color: !prevState.color,
-    }));
-    console.log(id);
-  };
-
   return (
     <div className="Container">
-      <h1>{toTitleCase(state.category)}</h1>
-      <div className="SearchBy">
-        <label>SortBy:</label>
-        <select
-          onChange={(e) =>
-            setState((prevState) => ({
-              ...prevState,
-              order: e.target.value,
-            }))
-          }
-          style={{ width: "16%", border: "1px solid black", marginLeft: "15px" }}
-        >
-          <option name="price" value="asc">
-            Price Low-High
-          </option>
-          <option name="price" value="desc">
-            Price High-Low
-          </option>
-        </select>
-      </div>
+      <h1>{state.category}</h1>
       {state.loading && (
         <Flex justifyContent={"center"}>
-          <Spinner
-            thickness="4px"
-            speed="0.65s"
-            emptyColor="gray.200"
-            color="blue.500"
-            size="xl"
-            mt="8%"
-          />
+          <Spinner size="xl" mt="8%" />
         </Flex>
       )}
       <div className="MugsData">
@@ -137,75 +86,28 @@ const Products = () => {
           state.data.length > 0 &&
           state.data.map((item) => (
             <div className="Mugs" key={item.id}>
-              <div>
-                <Icon
-                  className="heart"
-                  onClick={() => handleHeart(item.id)}
-                  backgroundColor={state.color ? "red" : null}
-                  cursor="pointer"
-                  m="5px"
-                  padding="2px"
-                  color={state.color ? "white" : "grey"}
-                  w={6}
-                  h={5}
-                  border="1px solid grey"
-                  borderRadius="50%"
-                  as={FiHeart}
-                />
-              </div>
-              <div>
-                <img alt="some" src={item.image} />
-              </div>
-              <span>Featured</span>
-              <div style={{ display: "flex" }}>
-                <h1>${item.price}</h1>
-                <Link to={`/products/${item.id}`}>
-                  <span className="icon">
-                    <ChevronDownIcon />
-                    More details
-                  </span>
-                </Link>
-              </div>
-              {item.count && (
-                <Flex ml={"10px"}>
-                  {[...Array(item.count)].map((_, index) => (
-                    <img
-                      key={index}
-                      alt="rating-star"
-                      src="https://ak1.ostkcdn.com/img/mxc/20200227_rating-star-full.svg"
-                    />
-                  ))}
-                </Flex>
-              )}
-              <div>
-                <p>{item.name}</p>
-              </div>
-              <div>
-                <h3>
-                  <CheckCircleIcon />
-                  Free Shipping
-                </h3>
-              </div>
+              <img alt="some" src={item.image} />
+              <h1>${item.price}</h1>
+              <p>{item.name}</p>
               {isAdmin && (
-                <div>
-                  <Button
-                    bg={"red"}
-                    width="60%"
-                    ml="20%"
-                    mb="20px"
-                    color="white"
-                    onClick={() => handleDelete(item.id)}
-                  >
-                    Delete
-                  </Button>
-                </div>
+                <Button
+                  bg={"red"}
+                  color="white"
+                  onClick={() => handleDelete(item.id)}
+                >
+                  Delete
+                </Button>
               )}
             </div>
           ))}
       </div>
 
-      <div style={{ margin: "auto", marginTop: "50px", marginBottom: "50px" }}>
-        <Button isDisabled={state.page <= 1} onClick={() => setState((prevState) => ({ ...prevState, page: prevState.page - 1 }))}>
+      {/* Pagination Controls */}
+      <Flex justifyContent="center" mt="20px" mb="50px">
+        <Button
+          isDisabled={state.page <= 1}
+          onClick={() => setState((prevState) => ({ ...prevState, page: prevState.page - 1 }))}
+        >
           <ChevronLeftIcon />
         </Button>
         <Button bg={"lightblue"}>{state.page}</Button>
@@ -215,7 +117,7 @@ const Products = () => {
         >
           <ChevronRightIcon />
         </Button>
-      </div>
+      </Flex>
     </div>
   );
 };
